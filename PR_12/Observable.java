7f3b4f3cc948e9d89d278993813922483e4f4689
annistasiuk <116899834+annistasiuk@users.noreply.github.com>
2025-04-24 10:31:45 +0300

    Delete PR_12 directory

--- PR_12/Observable.java
+++
@@ -1,7 +0,0 @@
-package PR_12;
-
-public interface Observable {
-    void addSubscriber(Observer observer);
-    void removeSubscriber(Observer observer);
-    void notifySubscribers();
-}
--- PR_12/Observer.java
+++
@@ -1,5 +0,0 @@
-package PR_12;
-
-public interface Observer {
-    void update(String creatorNick, int totalPosts);
-}
